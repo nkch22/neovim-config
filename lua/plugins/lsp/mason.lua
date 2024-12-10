8022dff44afdb9bce00940f8101c0a69c9052f6d
nkch22 <n.kriachko@proton.me>
2024-12-10 17:36:45 +0200

    add basic nvim config with lsp

---
+++ lua/plugins/lsp/mason.lua
@@ -0,0 +1,30 @@
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		require("mason").setup()
+
+		require("mason-lspconfig").setup({
+			automatic_installation = true,
+			ensure_installed = {
+				"jsonls",
+				"pyright",
+				"clangd",
+				"zls",
+			},
+		})
+
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint",
+				"clang-format",
+			},
+		})
+	end,
+}
---
+++ lua/plugins/treesiter.lua
@@ -0,0 +1,59 @@
+--- ~/nvim/lua/slydragonn/plugins/treesiter.lua
+
+return {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+    },
+    config = function()
+        local treesitter = require("nvim-treesitter.configs")
+
+        treesitter.setup({
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = { enable = true },
+            autotag = {
+                enable = true,
+            },
+            ensure_installed = {
+                "json",
+                "yaml",
+                "markdown",
+                "markdown_inline",
+                "bash",
+                "lua",
+                "vim",
+                "dockerfile",
+                "gitignore",
+                "c",
+                "cpp",
+                "glsl",
+                "rust",
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+            rainbow = {
+                enable = true,
+                disable = { "html" },
+                extended_mode = false,
+                max_file_lines = nil,
+            },
+            context_commentstring = {
+                enable = true,
+                enable_autocmd = false,
+            },
+        })
+    end,
+}
+
